b38eca2d29ae08b665c5120424217da6472cef4a
Dmitry Likholetov <likholetov1990@gmail.com>
2020-03-29 19:04:34 +0300

    hard4

--- script.js
+++ script.js
@@ -1,40 +1,18 @@
 'use strict';
 
-let lang = confirm('ru or en?'),
-    weekRu = ['понедельник', ' вторник', ' среда', ' четверг', ' пятница', ' суббота', ' воскресенье'],
-    weekEn = ['monday', ' tuesday', ' wednesday', ' thursday', ' friday', ' saturday', ' sunday'],
-    weeks = [weekRu, weekEn];
-console.log('lang: ', lang);
+let quest  = '     qwertyuiop qwertyuiop qwertyuiop   ';
+let someFunc = function(argument){
+    if (isNaN(argument)) {
+        let arg = argument.trim();
+        if (arg.length > 30) {
+            console.log('Больше 30');
+            console.log(arg.substring(0, 30) + '...');
+        } else {
+            console.log(arg);
+        }
+    } else {
+        console.log('Это не строка!');
+    }
+};
 
-        // Через IF ELSE
-/*
-if (lang) {
-    lang = 'ru';
-    console.log('lang: ', lang);
-    alert(weekRu);
-} else {
-    lang = 'en';
-    console.log('lang: ', lang);
-    alert(weekEn);
-}
-        // Через SWITCH CASE
-switch (lang){
-    case true:
-        lang = 'ru';
-        console.log('lang: ', lang);
-        alert(weekRu);
-        break;
-    case false:
-        lang = 'en';
-        console.log('lang: ', lang);
-        alert(weekEn);
-}
-*/
-        // Через тернарный оператор и многомерный массив
-let result = lang ? alert(weeks[0]) : alert(weeks[1]);
-
-        // Вторая часть задания
-const namePerson = prompt('who are you?');
-namePerson === 'Артем' ? console.log('директор') :
-     namePerson === 'Максим' ? console.log('преподаватель') :
-     console.log('студент');
+someFunc(quest);
