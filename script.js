c5a9507794c071b507ce97e85050a7496a0441a6
Dmitry Likholetov <likholetov1990@gmail.com>
2020-04-28 15:31:54 +0300

    hard5

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 'use strict';
 
-let quest  = '     qwertyuiop qwertyuiop qwertyuiop   ';
-let someFunc = function(argument){
-    if (isNaN(argument)) {
-        let arg = argument.trim();
-        if (arg.length > 30) {
-            console.log('Больше 30');
-            console.log(arg.substring(0, 30) + '...');
-        } else {
-            console.log(arg);
+let arr = ['123', '456', '789', '547', '258', '369', '659'];
+
+let usl1 = 2,
+    usl2 = 4;
+
+let circle = function() {
+    for (let i = 0; i < arr.length; i++){
+        if (+arr[i][0] === usl1 || +arr[i][0] === usl2){
+            console.log(arr[i]);
         }
-    } else {
-        console.log('Это не строка!');
+    }
+};
+circle();
+
+
+let arrNumber = [];
+let table = function() {
+    for (let i = 2; i <= 100; i++){
+        
+        let k,
+            count = 0;
+        for (let j = 2; j <= i; j++){
+            if (j === i) {
+                arrNumber.push(j);
+                break;
+            }
+            k = i % j;
+            
+            if (k === 0) {
+                count++;
+            }
+            if (count >= 1){
+                break;
+            }
+        }
+    }
+    for (let i = 0; i <= arrNumber.length; i++){
+
+        console.log(arrNumber[i] + ': Делители этого числа: 1 и ' + arrNumber[i]);
     }
 };
 
-someFunc(quest);
+table();
+
